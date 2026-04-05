da5f91158e02ab92396686c45a4e27d32b266cb7
scruffiebaka <itsmew2468@gmail.com>
2026-04-05 15:40:03 +0530

    make it look prettyuh

--- js/pages/Creators.js
+++ js/pages/Creators.js
@@ -1,5 +1,4 @@
 import { fetchCreators } from '../content.js';
-
 import Spinner from '../components/Spinner.js';
 
 export default {
@@ -8,6 +7,7 @@ export default {
     data: () => ({
         creators: [],
         loading: true,
+        selected: 0,
         err: null
     }),
 
@@ -16,30 +16,68 @@ export default {
             <Spinner></Spinner>
         </main>
 
-        <main v-else class="page-creators">
-            <p v-if="err" class="error">{{ err }}</p>
+        <main v-else class="page-leaderboard-container">
+            <div class="page-leaderboard">
 
-            <table v-else class="board">
-                <tr>
-                    <th>#</th>
-                    <th>User</th>
-                    <th>Points</th>
-                    <th>Featured</th>
-                    <th>Best</th>
-                </tr>
+                <p v-if="err" class="error">{{ err }}</p>
 
-                <tr v-for="(c, i) in creators">
-                    <td>#{{ i + 1 }}</td>
-                    <td class="user">
-                        <span class="type-label-lg">{{ c.user }}</span>
-                    </td>
-                    <td>{{ c.points }}</td>
-                    <td>{{ c.featured.join(', ') }}</td>
-                    <td>{{ c.best }}</td>
-                </tr>
-            </table>
+                <!-- LEFT LIST -->
+                <div class="board-container">
+                    <table class="board">
+                        <tr 
+                            v-for="(c, i) in creators"
+                            :class="{
+                                'top-1': i === 0,
+                                'top-2': i === 1,
+                                'top-3': i === 2
+                            }"
+                        >
+                            <td class="rank">
+                                <p>#{{ i + 1 }}</p>
+                            </td>
+
+                            <td class="points">
+                                <p>{{ c.points }}</p>
+                            </td>
+
+                            <td class="user" :class="{ 'active': selected == i }">
+                                <button @click="selected = i">
+                                    <span class="type-label-lg">{{ c.user }}</span>
+                                </button>
+                            </td>
+                        </tr>
+                    </table>
+                </div>
+
+                <!-- RIGHT PANEL -->
+                <div class="player-container">
+                    <div class="player" v-if="creator">
+                        <h1>#{{ selected + 1 }} {{ creator.user }}</h1>
+                        <h3>{{ creator.points }} points</h3>
+
+                        <h2>Best Awarded Level</h2>
+                        <p>{{ creator.best }}</p>
+
+                        <h2>Featured Levels ({{ creator.featured.length }})</h2>
+                        <table class="table">
+                            <tr v-for="lvl in creator.featured">
+                                <td>
+                                    <p>{{ lvl }}</p>
+                                </td>
+                            </tr>
+                        </table>
+                    </div>
+                </div>
+
+            </div>
         </main>
     `,
+
+    computed: {
+        creator() {
+            return this.creators[this.selected];
+        }
+    },
 
     async mounted() {
         const creators = await fetchCreators();
