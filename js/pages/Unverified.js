4fb083b10c2298e9b3bd70da8bb5909c8ab7e0fa
scruffiebaka <itsmew2468@gmail.com>
2026-04-15 16:30:42 +0530

    fix 2

--- js/pages/Unverified.js
+++ js/pages/Unverified.js
@@ -96,42 +96,30 @@ export default {
                     <div class="errors" v-show="errors.length > 0">
                         <p class="error" v-for="error of errors">{{ error }}</p>
                     </div>
-
+                    
                     <template v-if="editors">
+
                         <h3>List Editors</h3>
+
                         <ol class="editors">
+
                             <li v-for="editor in editors">
-                                <img 
-                                    :src="\`/assets/\${roleIconMap[editor.role] || 'user'}\${store.dark ? '-dark' : ''}.svg\`" 
-                                    :alt="editor.role"
-                                >
-                                <a v-if="editor.link" class="type-label-lg link" target="_blank" :href="editor.link">
-                                    {{ editor.name }}
-                                </a>
+                                <img :src="\`/assets/\${roleIconMap[editor.role]}\${store.dark ? '-dark' : ''}.svg\`" :alt="editor.role">
+                                <a v-if="editor.link" class="type-label-lg link" target="_blank" :href="editor.link">{{ editor.name }}</a>
                                 <p v-else>{{ editor.name }}</p>
                             </li>
+                            
                         </ol>
+
                     </template>
 
-                    <h2>
-                        <a 
-                            href="https://docs.google.com/document/d/13Tmtj1G1ydiBz4_banBFvjvMiIXlnpYhOzq-GMohPxs/edit?usp=sharing" 
-                            target="_blank" 
-                            style="color: var(--color-primary)"
-                        >
-                            NARLL Guidelines
-                        </a>
-                    </h2>
-
+                    <h2><a href="https://docs.google.com/document/d/13Tmtj1G1ydiBz4_banBFvjvMiIXlnpYhOzq-GMohPxs/edit?usp=sharing" target="_blank" style="color: blue; text-decoration: underline;">NARLL Guidelines</a></h2>
                     <h3>Notes:</h3>
                     <p>
                         The NARLL Website is in beta, so expect some stuff to be unfinished or bugged.
                     </p>
                     <p>
-                        Want the old spreadsheet version of the list? Here: 
-                        <a href="https://docs.google.com/spreadsheets/d/1gsfQKeiUm-mlEayo3e4FskkvuFJtIPjF_ad18j9q9XI">
-                            spreadsheet
-                        </a>
+                        Want the old spreadsheet version of the list? Here: <a href="https://docs.google.com/spreadsheets/d/1gsfQKeiUm-mlEayo3e4FskkvuFJtIPjF_ad18j9q9XI" style="color: blue; text-decoration: underline;">spreadsheet</a>
                     </p>
                 </div>
             </div>
