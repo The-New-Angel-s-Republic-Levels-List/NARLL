01bd62437c867c5a2868b393210f73d16a6225d6
scruffiebaka <itsmew2468@gmail.com>
2026-04-15 16:17:57 +0530

    bugfix 3

--- js/pages/Unverified.js
+++ js/pages/Unverified.js
@@ -3,6 +3,14 @@ import { fetchEditors, fetchUnverifiedList } from "../content.js";
 
 import Spinner from "../components/Spinner.js";
 
+const roleIconMap = {
+    owner: "crown",
+    admin: "user-gear",
+    helper: "user-shield",
+    dev: "code",
+    trial: "user-lock",
+};
+
 export default {
     components: { Spinner },
 
@@ -74,8 +82,11 @@ export default {
                 <div v-else-if="selected == null" class="level center">
                     <h2>Unverified Levels</h2>
                     <p>The following levels on the left are unverified and thus cannot be on the list.</p>
-                    <p>Keep in mind, a lot of the levels may not be list worthy (i.e not on par with the current standards) as they were created a long time ago.
-                        Please confirm with a moderator before attempting to verify one.</p>
+                    <p>
+                        Keep in mind, a lot of the levels may not be list worthy (i.e not on par with the current standards) 
+                        as they were created a long time ago.
+                        Please confirm with a moderator before attempting to verify one.
+                    </p>
                     <p>Select a level to view details.</p>
                 </div>
             </div>
@@ -85,23 +96,42 @@ export default {
                     <div class="errors" v-show="errors.length > 0">
                         <p class="error" v-for="error of errors">{{ error }}</p>
                     </div>
+
                     <template v-if="editors">
                         <h3>List Editors</h3>
                         <ol class="editors">
                             <li v-for="editor in editors">
-                                <img :src="\`/assets/\${roleIconMap[editor.role]}\${store.dark ? '-dark' : ''}.svg\`" :alt="editor.role">
-                                <a v-if="editor.link" class="type-label-lg link" target="_blank" :href="editor.link">{{ editor.name }}</a>
+                                <img 
+                                    :src="\`/assets/\${roleIconMap[editor.role] || 'user'}\${store.dark ? '-dark' : ''}.svg\`" 
+                                    :alt="editor.role"
+                                >
+                                <a v-if="editor.link" class="type-label-lg link" target="_blank" :href="editor.link">
+                                    {{ editor.name }}
+                                </a>
                                 <p v-else>{{ editor.name }}</p>
                             </li>
                         </ol>
                     </template>
-                    <h2><a href="https://docs.google.com/document/d/13Tmtj1G1ydiBz4_banBFvjvMiIXlnpYhOzq-GMohPxs/edit?usp=sharing" target="_blank" style="color: var(--color-primary)">NARLL Guidelines</a></h2>
+
+                    <h2>
+                        <a 
+                            href="https://docs.google.com/document/d/13Tmtj1G1ydiBz4_banBFvjvMiIXlnpYhOzq-GMohPxs/edit?usp=sharing" 
+                            target="_blank" 
+                            style="color: var(--color-primary)"
+                        >
+                            NARLL Guidelines
+                        </a>
+                    </h2>
+
                     <h3>Notes:</h3>
                     <p>
                         The NARLL Website is in beta, so expect some stuff to be unfinished or bugged.
                     </p>
                     <p>
-                        Want the old spreadsheet version of the list? Here: <a href="https://docs.google.com/spreadsheets/d/1gsfQKeiUm-mlEayo3e4FskkvuFJtIPjF_ad18j9q9XI">spreadsheet</a>
+                        Want the old spreadsheet version of the list? Here: 
+                        <a href="https://docs.google.com/spreadsheets/d/1gsfQKeiUm-mlEayo3e4FskkvuFJtIPjF_ad18j9q9XI">
+                            spreadsheet
+                        </a>
                     </p>
                 </div>
             </div>
@@ -115,7 +145,8 @@ export default {
         selected: null,
         errors: [],
         search: "",
-        store
+        store,
+        roleIconMap
     }),
 
     computed: {
@@ -140,23 +171,27 @@ export default {
     },
 
     async mounted() {
-        this.list = await fetchUnverifiedList();
-        this.editors = await fetchEditors();
+        try {
+            this.list = await fetchUnverifiedList();
+            this.editors = await fetchEditors();
 
-        if (!this.list) {
-            this.errors = [
-                "Failed to load unverified list."
-            ];
-        } else {
-            this.errors.push(
-                ...this.list
-                    .filter(([_, err]) => err)
-                    .map(([_, err]) => `Failed to load level (${err}.json)`)
-            );
-        }
+            if (!this.list) {
+                this.errors.push("Failed to load unverified list.");
+            } else {
+                this.errors.push(
+                    ...this.list
+                        .filter(([_, err]) => err)
+                        .map(([_, err]) => `Failed to load level (${err}.json)`)
+                );
+            }
 
-        if (!this.editors) {
-            this.errors.push("Failed to load editors.");
+            if (!this.editors) {
+                this.errors.push("Failed to load editors.");
+            }
+
+        } catch (e) {
+            console.error(e);
+            this.errors.push("Unexpected error while loading page.");
         }
 
         this.loading = false;
