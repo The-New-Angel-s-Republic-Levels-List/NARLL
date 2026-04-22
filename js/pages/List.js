6b9585360937b0f76c35db6af697fe4afdf986cd
vexenous <vexenous@proton.me>
2026-04-22 18:24:08 +0000

    Update List.js

--- js/pages/List.js
+++ js/pages/List.js
@@ -178,7 +178,7 @@ export default {
                             </li>
                         </ol>
                     </template>
-                    <h2><a target="_blank" style="color: blue; text-decoration: underline;">NARLL Guidelines</a></h2>
+                    <h2><a>NARLL Guidelines will be back shortly</a></h2>
                     <h3>Notes:</h3>
                     <p>
                         The NARLL Website is in beta, so expect some stuff to be unfinished or bugged.
