33e7a717b64485a1f66c37ef6dc569daadd3e0c3
scruffiebaka <itsmew2468@gmail.com>
2026-05-03 01:41:17 +0530

    make records not clickable

--- js/pages/List.js
+++ js/pages/List.js
@@ -155,7 +155,7 @@ export default {
                                 <p>{{ record.percent }}%</p>
                             </td>
                             <td class="user">
-                                <a :href="record.link" target="_blank" class="type-label-lg">{{ record.user }}</a>
+                                <p class="type-label-lg">{{ record.user }}</p>
                             </td>
                             <td class="mobile">
                                 <img v-if="record.mobile" :src="\`/assets/phone-landscape\${store.dark ? '-dark' : ''}.svg\`" alt="Mobile">
