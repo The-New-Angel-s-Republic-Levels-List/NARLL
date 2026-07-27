722200cf8530102dad3d621e52a66c9fe2a0da49
watty <bowifiedbow@gmail.com>
2026-07-27 11:27:12 -0600

    Update Structures.cs

--- NARLLGenerator/Structures.cs
+++ NARLLGenerator/Structures.cs
@@ -47,7 +47,7 @@ public class UnverifiedLevel
     public string progress { get; set; }
     public string wr_holder { get; set; }
     public string showcase { get; set; }
-    public string verification { get; set; }
+    public string creators { get; set; }
     public string notes {get; set;}
     public string nong {get; set;}
 }
